9becff8a164fdab4108b1f907d9fb1ff4131f190
Miha Jan Strehovec <48392708+mihajanstrehovec@users.noreply.github.com>
2023-03-30 12:57:03 +0200

    nextjs config

--- digitalizacija-turist-takse/next.config.js
+++ digitalizacija-turist-takse/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
