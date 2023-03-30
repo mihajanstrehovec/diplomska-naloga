9108dfdd8a51499578f31cc9049528dff91ffd84
Miha Jan Strehovec <48392708+mihajanstrehovec@users.noreply.github.com>
2023-03-30 14:17:51 +0200

    success page bg change

--- digitalizacija-turist-takse/pages/success.tsx
+++ digitalizacija-turist-takse/pages/success.tsx
@@ -2,6 +2,7 @@ import type {NextPage} from 'next'
 import Layout from '@/components/Layout'
 import { useRouter } from 'next/router'
 
+
 const Success: NextPage = () => {
 
   const router = useRouter()
